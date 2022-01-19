697977b346ae67fccf46ff3b93d006efdbb396ee
Nico Manzoni <nico@menzipics.ch>
2022-01-19 15:27:54 +0100

    C

--- practice/grades/grades.c
+++ practice/grades/grades.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int grades;
+    int grades[3];
     int average;
 
     grades[0] = 80;
-    /* TODO: define the missing grade
-       so that the average will sum to 85. */
+    grades[1] = 85;
     grades[2] = 90;
 
     average = (grades[0] + grades[1] + grades[2]) / 3;
