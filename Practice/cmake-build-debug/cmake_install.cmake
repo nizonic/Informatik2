da4b570c4d957236a1bb19a8cbe40c89a2e22de1
Nico Manzoni <nico@menzipics.ch>
2022-02-25 09:00:58 +0100

    renamed files

--- Practice/cmake-build-debug/cmake_install.cmake
+++ Practice/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/nicom/CLionProjects/Informatik2/practice
+# Install script for directory: C:/Users/nicom/CLionProjects/Informatik2/Practice
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
