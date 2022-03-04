0a254db227fc3f1aecf7057e790abd9c3a64ef4e
Nico Manzoni <nico@menzipics.ch>
2022-03-04 20:42:33 +0100

    pointy hat

--- Exercises/01/ex01_1.c
+++ Exercises/01/ex01_1.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 
+
 // hard-coded maximum length for input strings
 const int MAX_LENGTH = 1000;
 
@@ -16,12 +17,22 @@ void BS(char A[]) {
 	return;
 }
 
+
+
+int count_vowels(char A[]){
+    int i;
+    int count = 0;
+    for (i=0; A[i] != '\0'; i++){
+        if (A[i] == 'a' || A[i] == 'e' || A[i] == 'o' || A[i] == 'i' ||A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'O' || A[i] == 'I' ||A[i] == 'U'){count++;}
+    }
+    return 0;
+}
+
 int main() {
 	char input_string[MAX_LENGTH + 1];
 	printf("Enter a string: ");
 	scanf("%[^\n]s", input_string);
 
-	
 
 	return 0;
 }
