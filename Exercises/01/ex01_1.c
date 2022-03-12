19c88885624b5832bb759144dcc1b21c84b14085
Nico Manzoni <nico@menzipics.ch>
2022-03-12 09:03:33 +0100

    ex1.1 done

--- Exercises/01/ex01_1.c
+++ Exercises/01/ex01_1.c
@@ -12,9 +12,25 @@
 // hard-coded maximum length for input strings
 const int MAX_LENGTH = 1000;
 
-void BS(char A[]) {
+int is_vowel(char A){
+    if (A == 'a' || A == 'e' || A == 'o' || A == 'i' ||A == 'u'
+    || A == 'A' || A == 'E' || A == 'O' || A == 'I' ||A == 'U'){return 1;}
+    return 0;
+}
 
-	return;
+void BS(char A[]) {
+    char BS[MAX_LENGTH];
+    int i;
+    int add = 0;
+    for (i=0; A[i] != '\0'; i++){
+        if (is_vowel(A[i]) == 0){BS[i+add] = A[i];}
+        else {
+            BS[i+add] = A[i];
+            BS[i+add+1] = 'b';
+            BS[i+add+2] = A[i];
+            add += 2;}
+    }
+    printf("%s\n", BS);
 }
 
 
@@ -23,16 +39,17 @@ int count_vowels(char A[]){
     int i;
     int count = 0;
     for (i=0; A[i] != '\0'; i++){
-        if (A[i] == 'a' || A[i] == 'e' || A[i] == 'o' || A[i] == 'i' ||A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'O' || A[i] == 'I' ||A[i] == 'U'){count++;}
+        if (is_vowel(A[i]) == 1){count++;}
     }
-    return 0;
+    return count;
 }
 
 int main() {
 	char input_string[MAX_LENGTH + 1];
 	printf("Enter a string: ");
 	scanf("%[^\n]s", input_string);
-
+    printf("There are %d vowels in this string\n", count_vowels(input_string));
+    BS(input_string);
 
 	return 0;
 }
