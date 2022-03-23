72fc9d56244ae9be2f7cd4145143cddf51bdd36a
Nico Manzoni <nico@menzipics.ch>
2022-03-23 10:43:44 +0100

    added Ex02 files

---
+++ Exercises/02/ex02_2.c
@@ -0,0 +1,21 @@
+/****************************************************************************
+ * University of Zurich, Informatics II, Spring Semester 2022               *
+ * Exercise 2 - Task 3                                                      *
+ *                                                                          *
+ * @author Nico Manzoni                                                     *
+ * @date 13.03.2022                                                         *
+ ****************************************************************************/
+
+#include <stdio.h>
+
+// TODO: your implementation
+
+int main() {
+	int input;
+	printf("Enter the number of blinks: ");
+	scanf("%d", &input);
+
+	// TODO: your implementation
+
+	return 0;
+}
