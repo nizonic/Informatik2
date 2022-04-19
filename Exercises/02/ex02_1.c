57787f407ea82a92f8546ea349731a905076ecdc
Nico Manzoni <nico@menzipics.ch>
2022-04-19 14:13:17 +0200

    failed implementation of recursive algo

--- Exercises/02/ex02_1.c
+++ Exercises/02/ex02_1.c
@@ -11,7 +11,29 @@
 // hard-coded maximum length for input strings
 const int MAX_LENGTH = 1000;
 
-// TODO: your implementation
+int maximum2(int a, int b){
+    if (a>=b)return a;
+    return b;
+}
+
+int secondSmallest(int array[], int length){
+    int largestElement;
+    int secondLargestElement;
+    int left = 0;
+    int right = length;
+    int middle = length / 2;
+    int leftBig;
+    int rightBig;
+    if (length == 1){return array[0];}
+    leftBig = secondSmallest(&array[left, middle], middle);
+    rightBig = secondSmallest(&array[middle+1, right], length-middle+1);
+    for (int i=1; i<length; i++){
+        if (array[i] >= array[i-1]){
+            largestElement = array[i];
+        }
+    }
+    return maximum2(leftBig, rightBig);
+}
 
 int main() {
 	printf("Values of array separated by spaces (non-number to stop): ");
@@ -21,7 +43,7 @@ int main() {
 		pos++;
 	}
 	// variable pos will contain number of integers read in from user
-
+    printf("%d\n", secondSmallest(arr, pos));
 	// TODO: your implementation
 
 	return 0;
