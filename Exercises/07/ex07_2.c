21652434b8878b14fb476777da30d4f0679ca7b8
Nico Manzoni <nico@menzipics.ch>
2022-04-26 09:05:41 +0200

    added stack functions

---
+++ Exercises/07/ex07_2.c
@@ -0,0 +1,170 @@
+/****************************************************************************
+ * University of Zurich, Informatics II, Spring Semester 2022               *
+ * Exercise 7 - Task 2                                                      *
+ *                                                                          *
+ * @author Nico Manzoni                                                     *
+ * @date 26.04.2022                                                         *
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Stack {
+	unsigned int capacity;
+	int* items;
+  int top;
+} Stack;
+
+Stack* create(unsigned int capacity) {
+    Stack* new_stack = malloc(sizeof(Stack));
+    if (!new_stack) {
+        return NULL;
+    }
+    new_stack->capacity = capacity;
+    new_stack->top = -1;
+    new_stack->items = malloc(capacity * sizeof(int));
+    return new_stack;
+}
+
+void delete(struct Stack* stack){
+    free(stack->items);
+    free(stack);
+}
+
+int is_empty(Stack* stack) {
+    if (stack->top == -1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int is_full(Stack* stack){
+    if (stack->top == stack->capacity - 1){
+        return 1;
+    }
+    else return 0;
+}
+
+int get_capacity(struct Stack* stack){
+    return stack->capacity -1;
+}
+
+int num_items(struct Stack* stack){
+    return stack->top + 1;
+}
+
+int pop(struct Stack* stack){
+    if (is_empty(stack)){
+        printf("Error: Could not pop item. Stack is empty\n");
+        return -1;
+    }
+    stack->top--;
+    return stack->items[stack->top];
+}
+
+int push(struct Stack* stack, int item){
+    if (is_full(stack)){
+        printf("Error: Could not push item %d onto stack. Stack is full.\n", item);
+        return -1;
+    }
+    stack->top++;
+    stack->items[stack->top] = item;
+    printf("Item %d pushed to stack.\n", item);
+    return 0;
+}
+
+int peek(struct Stack* stack){
+    if (is_empty(stack)){
+        printf("Stack is empty.\n");
+        return -1;
+    }
+    printf("Item at the top of the stack is %d\n", stack->items[stack->top]);
+    return stack->items[stack->top];
+}
+
+void print(struct Stack* stack) {
+    if (is_empty(stack)) {
+        printf("Stack is empty.\n");
+    }
+    else {
+        printf("Printing stack:\n");
+        for (int i = 0; i <= stack->top; i++) {
+            printf("%d\n", stack->items[i]);
+        }
+    }
+}
+
+int is_equal(struct Stack* stack1, struct Stack* stack2){
+    if (num_items(stack1) != num_items(stack2)){
+        printf("The stacks are not equal.\n");
+        return -1;
+    }
+    for (int i = 0; i <= num_items(stack1); i++){
+        if (stack1->items[i] != stack2->items[i]) {
+            printf("The stacks are not equal.\n");
+            return -1;
+        }
+    }
+    printf("The Stacks are equal.\n");
+    return 1;
+}
+
+void reverse(struct Stack* stack){
+    int n = num_items(stack);
+    printf("Reversing stack.\n");
+    for (int i = 0; i < n / 2; i++) {
+        int temp = stack->items[i];
+        stack->items[i] = stack->items[n - i - 1];
+        stack->items[n - i - 1] = temp;
+    }
+}
+
+int main() {
+    Stack* s1 = create(6);
+    Stack* s2 = create(6);
+    push(s1, 1);
+    push(s1, 3);
+    push(s1, 5);
+    push(s1, 7);
+    push(s1, 42);
+    push(s1, 17);
+    print(s1);
+    print(s2);
+    printf("Check whether s1 is full: %d\n", is_full(s1));
+    printf("Check whether s2 is full: %d\n", is_full(s2));
+    printf("Number of elements in s1: %d\n", num_items(s1));
+    printf("Number of elements in s2: %d\n", num_items(s2));
+    printf("Overflow test... ");
+    push(s1, 99);
+    printf("Element popped from s1: %d\n", pop(s1));
+    printf("Element popped from s1: %d\n", pop(s1));
+    printf("Element peeked from s1: %d\n", peek(s1));
+    for (int i = 0; i < 4; i++) {
+        push(s2, i + i + 1);
+    }
+    print(s1);
+    print(s2);
+    printf("Comparison of s1 with s2: %d\n", is_equal(s1, s2));
+    printf("Element popped from s2: %d\n", pop(s2));
+    printf("Comparison of s1 with s2: %d\n", is_equal(s1, s2));
+    for (int i = 0; i < 4; i++) {
+        pop(s1);
+    }
+    print(s1);
+    print(s2);
+    printf("Check whether s1 is empty: %d\n", is_empty(s1));
+    printf("Check whether s2 is empty: %d\n", is_empty(s2));
+    printf("Underflow test... ");
+    pop(s1);
+    printf("Underflow test... ");
+    peek(s1);
+    push(s2, 9);
+    push(s2, 8);
+    push(s2, 2);
+    print(s2);
+    reverse(s2);
+    print(s2);
+    return 0;
+}
