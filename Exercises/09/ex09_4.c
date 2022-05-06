e9f076ad7155797df781902f4f90f53ca0296ee8
Nico Manzoni <nico@menzipics.ch>
2022-05-06 12:17:55 +0200

    ex9_4 completed

---
+++ Exercises/09/ex09_4.c
@@ -0,0 +1,31 @@
+/****************************************************************************
+ * University of Zurich, Informatics II, Spring Semester 2022               *
+ * Exercise 9 - Task 4                                                      *
+ *                                                                          *
+ * @author Nico Manzoni                                                     *
+ * @date 06.05.2022                                                         *
+ ****************************************************************************/
+
+struct node{
+    struct node* parent;
+    struct node* lft;
+    struct node* rgt;
+};
+
+void rightRotate(struct node* temp){
+    struct node* g = temp->parent;
+    struct node* left = temp->lft;
+    struct node* b = temp->lft;
+    if (left->rgt){
+        temp = b->parent;
+    }
+    g = left->parent;
+    if (temp == g->lft){
+        g->lft = left;
+    }
+    else{
+        g->rgt = left;
+    }
+    left->rgt = temp;
+    temp->parent = left;
+}
